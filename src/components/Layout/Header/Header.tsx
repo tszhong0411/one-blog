7e5990271bd5ca3a88d3725afbe6b169cd415622
tszhong0411 <lainelson411@gmail.com>
2023-04-01 00:01:02 +0800

    fix: 修復錯誤的 CSS

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -10,7 +10,7 @@ const Header = async () => {
   const session = await getServerSession(authOptions)
 
   return (
-    <header className='fixed top-0 left-0 right-0 z-40 bg-white/80 shadow-sm saturate-[1.8] backdrop-blur-[10px] dark:bg-black/50 dark:saturate-100'>
+    <header className='fixed top-0 left-0 right-0 z-40 shadow-sm backdrop-blur-[10px] bg-black/50 saturate-100'>
       <div className='mx-auto flex h-[60px] max-w-4xl items-center justify-between px-8'>
         <Link href='/' className='text-lg font-bold'>
           One Blog
