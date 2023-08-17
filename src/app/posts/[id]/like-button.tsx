ea4f4e2288689e5861000e4794d6e09a76b66d86
tszhong0411 <lainelson411@gmail.com>
2023-08-17 23:34:38 +0800

    perf: optimize like button

--- src/app/posts/[id]/like-button.tsx
+++ src/app/posts/[id]/like-button.tsx
@@ -1,29 +1,56 @@
 'use client'
 
+import { createId } from '@paralleldrive/cuid2'
 import { Like } from '@prisma/client'
 import { IconHeart } from '@tabler/icons-react'
 import { Button } from '@tszhong0411/ui'
 import { cx } from '@tszhong0411/utils'
 import { User } from 'next-auth'
+import React from 'react'
 import { toast } from 'react-hot-toast'
 
 import { likePost, unlikePost } from '@/actions'
 
 type LikeButtonProps = {
-  likes: Pick<Like, 'id' | 'userId'>[]
+  likes: Like[]
   user: User | undefined
   postId: string
 }
 
 const LikeButton = (props: LikeButtonProps) => {
   const { likes, user, postId } = props
+  const [optimisticLikes, updateOptimisticLike] =
+    React.experimental_useOptimistic<Like[], 'CREATE' | 'DELETE'>(
+      likes,
+      (state, action) => {
+        if (action === 'DELETE') {
+          return state.filter(
+            (like) => like.userId !== user?.id && like.postId !== postId,
+          )
+        }
 
-  const isUserLiked = likes.some((like) => like.userId === user?.id)
+        return [
+          ...state,
+          {
+            id: createId(),
+            userId: user ? user.id : createId(),
+            postId,
+          },
+        ]
+      },
+    )
+
+  const isUserLiked = optimisticLikes.some((like) => like.userId === user?.id)
 
   const handleLike = async () => {
     try {
-      !isUserLiked && (await likePost(postId))
-      isUserLiked && (await unlikePost(postId))
+      if (!isUserLiked) {
+        updateOptimisticLike('CREATE')
+        await likePost(postId)
+      } else if (isUserLiked) {
+        updateOptimisticLike('DELETE')
+        await unlikePost(postId)
+      }
     } catch (error) {
       toast.error((error as Error).message)
     }
@@ -37,11 +64,10 @@ const LikeButton = (props: LikeButtonProps) => {
       onClick={handleLike}
     >
       <IconHeart
-        width={20}
-        height={20}
+        size={20}
         className={cx(isUserLiked && 'fill-red-500 text-red-500')}
       />
-      {likes.length}
+      {optimisticLikes.length}
     </Button>
   )
 }
