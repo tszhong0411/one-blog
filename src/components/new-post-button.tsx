b96c3e1fc7878992e7c7a30a28e46e38d895ba0c
tszhong0411 <lainelson411@gmail.com>
2023-08-17 23:34:09 +0800

    feat: add loading state to create new post button

--- src/components/new-post-button.tsx
+++ src/components/new-post-button.tsx
@@ -3,26 +3,36 @@
 import { IconPencilPlus } from '@tabler/icons-react'
 import { Button } from '@tszhong0411/ui'
 import { useRouter } from 'next/navigation'
+import React from 'react'
 import { toast } from 'react-hot-toast'
 
 import { createNewPost } from '@/actions'
 
 const NewPostButton = () => {
   const router = useRouter()
+  const [isPending, startTransition] = React.useTransition()
 
-  const newPost = async () => {
-    try {
-      const postId = await createNewPost('Untitled post')
-      router.refresh()
-      router.push(`/editor/${postId}`)
-    } catch (error) {
-      toast.error((error as Error).message)
-    }
+  const newPost = () => {
+    startTransition(async () => {
+      try {
+        const postId = await createNewPost('Untitled post')
+        router.refresh()
+        router.push(`/editor/${postId}`)
+      } catch (error) {
+        toast.error((error as Error).message)
+      }
+    })
   }
 
   return (
-    <Button variant='ghost' className='py-1.5' onClick={newPost}>
-      <IconPencilPlus width={20} height={20} className='mr-2' />
+    <Button
+      variant='ghost'
+      className='py-1.5'
+      onClick={newPost}
+      loading={isPending}
+      disabled={isPending}
+    >
+      <IconPencilPlus size={20} className='mr-2' />
       Write
     </Button>
   )
