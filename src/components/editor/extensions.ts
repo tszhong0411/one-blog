69358cdb7a9111c3dc01c10474e96754f24f7774
tszhong0411 <me@honghong.me>
2025-02-02 19:05:11 +0800

    feat: support code block

--- src/components/editor/extensions.ts
+++ src/components/editor/extensions.ts
@@ -1,20 +1,20 @@
 import type { AnyExtension } from '@tiptap/react'
 
+import { CodeBlockLowlight } from '@tiptap/extension-code-block-lowlight'
 import { Highlight } from '@tiptap/extension-highlight'
 import { Link } from '@tiptap/extension-link'
 import { Placeholder } from '@tiptap/extension-placeholder'
 import { TaskItem } from '@tiptap/extension-task-item'
 import { TaskList } from '@tiptap/extension-task-list'
 import { StarterKit } from '@tiptap/starter-kit'
+import { all, createLowlight } from 'lowlight'
 import { Markdown } from 'tiptap-markdown'
+
+const lowlight = createLowlight(all)
 
 export const extensions: AnyExtension[] = [
   StarterKit.configure({
-    codeBlock: {
-      HTMLAttributes: {
-        class: 'p-3'
-      }
-    }
+    codeBlock: false
   }),
   Highlight,
   TaskList,
@@ -30,5 +30,12 @@ export const extensions: AnyExtension[] = [
   }).extend({
     inclusive: false,
     priority: 100
+  }),
+  CodeBlockLowlight.configure({
+    lowlight,
+    defaultLanguage: 'plaintext',
+    HTMLAttributes: {
+      class: 'p-3 not-prose bg-secondary/50 rounded-lg border my-6'
+    }
   })
 ]
